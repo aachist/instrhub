1d913a9745395c7953cf2405944ea92a20392471
aachist <aachist@gmail.com>
2025-08-25 20:12:15 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿const App = () => {
+  return React.createElement("div", null,
+    React.createElement("h1", null, "Галерея Instrhub"),
+    React.createElement("img", { src: "assets/example.png", alt: "Example" })
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(App));
